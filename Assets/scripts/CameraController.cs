6f64d81f2bc77f0220d3cde2141e0968b093c2cf
Cuello <Erick.165013@ad.ccsd.net>
2024-02-07 13:13:46 -0800

    Fixed camera (mostly)

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -2,20 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
     public GameObject player;
     private Vector3 dist;
-    void Start()
-    {
+
+    //Rotation Sensitivity
+    public float minAngle = -90f;
+    public float maxAngle = 90f;
+    public float Sens = 50;
+
+    private float rotationX = 0;
+    private float rotationY = 0;
+  
+    //Rotation Value
+
+    void Start() {
         dist.x = transform.position.x - player.transform.position.x;
         dist.y = transform.position.y - player.transform.position.y - 1.5f;
         dist.z = transform.position.z - player.transform.position.z;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.position = player.transform.position;
+    void Update() {
+        transform.position = player.transform.position - dist;
+
+        float xr = Input.GetAxis("Mouse X") * Time.deltaTime * Sens;
+        float yr = Input.GetAxis("Mouse Y") * Time.deltaTime * Sens;
+        rotationX += yr;
+        rotationY += xr;
+
+        transform.rotation = Quaternion.Euler(new Vector3(rotationX, rotationY, 0));
+
+        if(rotationX > maxAngle){
+            rotationX = maxAngle;
+        }
+        else if(rotationX < minAngle) {
+            rotationX = minAngle;
+        }
     }
 }
--- Assets/scripts/playerHandler.cs
+++ Assets/scripts/playerHandler.cs
@@ -6,6 +6,7 @@ public class playerHandler : MonoBehaviour {
     // Start is called before the first frame update
     float speed;
     public GameObject cam;
+    public GameObject tracker;
     void Start() {
         speed = 25f;
         Cursor.lockState = CursorLockMode.Locked;
@@ -20,27 +21,26 @@ public class playerHandler : MonoBehaviour {
 
     void move() {
         if(Input.GetKey(KeyCode.W)) {
-            transform.Translate(cam.transform.forward * speed * Time.deltaTime);
+            transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
         }
         if(Input.GetKey(KeyCode.S)) {
-            transform.Translate(cam.transform.forward * -speed * Time.deltaTime);
+            transform.Translate(transform.forward * -speed * Time.deltaTime, Space.World);
         }
         if(Input.GetKey(KeyCode.A)) {
-            transform.Translate(cam.transform.right * -speed * Time.deltaTime);
+            transform.Translate(transform.right * -speed * Time.deltaTime, Space.World);
         }
         if(Input.GetKey(KeyCode.D)) {
-            transform.Translate(cam.transform.right * speed * Time.deltaTime);
+            transform.Translate(transform.right * speed * Time.deltaTime, Space.World);
         }
 
 
         //cam
-        float xr = Input.GetAxis("Mouse X");
-        float yr = Input.GetAxis("Mouse Y");
+        float xr = Input.GetAxis("Mouse X") * Time.deltaTime * cam.GetComponent<CameraController>().Sens;
+        float yr = Input.GetAxis("Mouse Y") * Time.deltaTime * cam.GetComponent<CameraController>().Sens;
 
         transform.Rotate(new Vector3(0, xr, 0));
         cam.transform.Rotate(new Vector3(yr, xr, 0));
-
-        cam.transform.rotation = Quaternion.Euler(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, 0);
+        cam.transform.eulerAngles = new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, 0);
 
     }
 }
